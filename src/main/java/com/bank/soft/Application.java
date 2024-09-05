03c062c6c3735aa9816c7eb9e5401a7a28a61e0a
Igor-Sanches <34573915+Igor-Sanches@users.noreply.github.com>
2024-09-05 01:24:16 -0300

    Bugs Fits CORs

--- src/main/java/com/bank/soft/Application.java
+++ src/main/java/com/bank/soft/Application.java
@@ -1,11 +1,14 @@
 package com.bank.soft;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server url")})
 @SpringBootApplication
 public class Application {
 
